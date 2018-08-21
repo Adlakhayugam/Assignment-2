80db46ee43cf8c899dc81fa06aadb7db1ccf223e
Adlakhayugam <adlakhayugam17@gmail.com>
2018-08-21 09:34:47 +0530

    this is 2nd assignment

---
+++ Assignment-2.py
@@ -0,0 +1,39 @@
+"""Question 1"""
+print("Hello World")
+
+
+"""Question 2"""
+s1="Hello"
+s2="World"
+print(s1+" "+s2)
+
+
+"""Question 3"""
+v1=input("Enter First Name:- ")
+v2=input("Enter Last Name:- ")
+v3=int(input("Enter Age:- "))
+print("Name:- ",v1+" "+v2)
+print("Age:- {}".format(v3))
+
+
+"""Question 4"""
+print('''"Let's Get Started"''')
+
+
+"""Question 5"""
+s="Acadview" 
+course="Python" 
+fees=5000 
+
+print("""S:-%s
+Fees:- %d
+Course : %s
+"""%(s,fees,course))
+
+
+
+"""Question 6"""
+pi=3.14
+r=float(input("Enter Radius of circle"))
+area=pi*(r**2)
+print("Area of circle:- {}".format(area))
